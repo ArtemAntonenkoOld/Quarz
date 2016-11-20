b7a5c824ff2524a5de0cba6088716bb2bd8112b2
GoGo228 <ghhfghhf5@gmail.com>
2016-11-20 11:57:54 +0200

    Update HiSheduler.cs

--- HiSheduler.cs
+++ HiSheduler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using Quartz;
+using Quartz.Impl;
+using StudNet.BL.AutoUpdater.Triggers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class HiSheduler
+    public static class HiSheduler
     {
+             public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<RozkladTrigger>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()  // создаем триггер
+                .WithIdentity("trigger1", "group1")     // идентифицируем триггер с именем и группой
+                .StartNow()                            // запуск сразу после начала выполнения
+                .WithSimpleSchedule(x => x            // настраиваем выполнение действия
+                    .WithIntervalInSeconds(1)          // через 1 минуту
+                    .RepeatForever())                   // бесконечное повторение
+                .Build();                               // создаем триггер
+
+            scheduler.ScheduleJob(job, trigger);
+        }   
     }
 }
