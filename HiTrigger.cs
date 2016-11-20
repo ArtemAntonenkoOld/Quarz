c2c5ba0bbd427f9468cd6da76a77b1a48a192141
GoGo228 <ghhfghhf5@gmail.com>
2016-11-20 11:53:08 +0200

    20.11.2016

---
+++ HiTrigger.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+using System;
+
+public class RozkladTrigger : IJob
+{
+    public int a = 1;
+
+    public void Execute(IJobExecutionContext context)
+    {
+       
+            Console.Write("harasho");
+     }
+}
